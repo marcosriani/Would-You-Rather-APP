d43749ef7f70c6c6cc103cc4b9d9a40fb07c2da4
Marcos Riani <marcosriani@hotmail.com>
2020-07-27 21:46:08 +0100

    Poll componet showing up correct avatar image - Working on the css and after the logic to select the questions.

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -2,26 +2,46 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import './Poll.css';
+
 class Poll extends Component {
   render() {
-    const { username, question, imgUrl } = this.props;
+    const { username, questionOne, questionTwo, imgUrl, id } = this.props;
 
     console.log(imgUrl);
     return (
-      <div>
+      <div className='poll-wrap'>
         <h3> {username} asked:</h3>
-        <div className='card-poll-info'>
-          <div className='card-poll-img'>
-            <img src={imgUrl} alt={`${username} avatar`} />
+        <div className='poll-data'>
+          <div className='poll-img'>
+            <img src={imgUrl.slice(1)} alt={`${username} avatar`} />
           </div>
-          <div className='card-poll-data'>
+          <div>
             <div>
               <h4>Would you rather</h4>
-              <p>{question}</p>
-              {/* <p>{question.optionTwo.text}</p> */}
-            </div>
-            <div className='card-poll-btn'>
-              <button>Answer Poll</button>
+              <form className='form-data'>
+                <div>
+                  <input
+                    type='radio'
+                    id='questionOne'
+                    name='gender'
+                    value={questionOne}
+                  />
+                  <label htmlFor='questionOne'>{questionOne}</label>
+                  <br />
+                  <input
+                    type='radio'
+                    id='questionTwo'
+                    name='gender'
+                    value={questionTwo}
+                  />
+                  <label htmlFor='questionTwo'>{questionTwo}</label>
+                </div>
+
+                <div className='poll-button-wrap'>
+                  <button className='poll-button'>Answer Poll</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -44,12 +64,15 @@ const mapStateToProps = ({ users, questions }, onwProps) => {
     username:
       Object.entries(questions).length !== 0 &&
       users[questions[id].author].name,
-    question:
+    questionOne:
       Object.entries(questions).length !== 0 && questions[id].optionOne.text,
+    questionTwo:
+      Object.entries(questions).length !== 0 && questions[id].optionTwo.text,
     imgUrl:
       Object.entries(questions).length !== 0
         ? users[questions[id].author].avatarURL.toString()
         : '',
+    id,
   };
 };
 
